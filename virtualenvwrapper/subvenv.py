6a73c726fd15d4bb26e261786fa9926bad533b3c
Giulia Vergottini <hello@railslide.io>
2014-09-28 17:10:07 +0200

    fix project_folder and project_venv paths

--- virtualenvwrapper/subvenv.py
+++ virtualenvwrapper/subvenv.py
@@ -7,13 +7,13 @@ log = logging.getLogger(__name__)
 
 
 def post_mkproject(args):
-    project_name = os.getenv('PWD')
+    project_name = os.path.split(os.getenv('PWD'))[1]
     project_venv = os.path.join(os.getenv('WORKON_HOME'), project_name)
     project_path_file = os.path.join(project_venv, '.project')
     project_interpreter = os.path.join(project_venv, 'bin/python')
 
     with open(project_path_file, 'r') as f:
-        project_folder = f.readline()
+        project_folder = f.readline().rstrip('\r\n')
 
     sublime_file_name = "{}.sublime-project".format(project_name)
     settings_text = {
@@ -31,3 +31,7 @@ def post_mkproject(args):
         f.write(json.dumps(settings_text, indent=4))
 
     return
+
+
+if __name__ == '__main__':
+    post_mkproject('hello')
