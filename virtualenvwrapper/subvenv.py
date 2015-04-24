16e54b56c4199e9816f974da91bf1059c8a93098
Giulia Vergottini <hello@railslide.io>
2015-04-24 07:54:49 +0200

    add more general make_project function

--- virtualenvwrapper/subvenv.py
+++ virtualenvwrapper/subvenv.py
@@ -13,7 +13,7 @@ def post_mkproject(args=None):
     project_venv = os.getenv('VIRTUAL_ENV')
     project_name = os.path.basename(project_venv)
     project_path_file = os.path.join(project_venv, '.project')
-    project_interpreter = os.path.join(project_venv, 'bin/python')
+    interpreter = os.path.join(project_venv, 'bin/python')
 
     try:
         with open(project_path_file, 'r') as f:
@@ -22,10 +22,27 @@ def post_mkproject(args=None):
         sys.stderr.write('Virtualenv project not found.\n')
         return
 
-    create_sublime_project(project_folder, project_name, project_interpreter)
+    create_sublime_project_file(project_folder, project_name, interpreter)
 
 
-def create_sublime_project(project_folder, project_name, interpreter, *args):
+def make_project(path):
+    if not path:
+        target_folder = os.getcwd()
+
+    venv_path = os.getenv('VIRTUAL_ENV')
+
+    if not venv_path:
+        sys.stderr.write(
+            'You need to be inside a virtualenv for using subvenv.')
+        sys.exit(2)
+
+    project_name = os.path.basename(venv_path)
+    interpreter = sys.executable
+
+    create_sublime_project_file(target_folder, project_name, interpreter)
+
+
+def create_sublime_project_file(project_folder, project_name, interpreter):
     sublime_file_name = "{}.sublime-project".format(project_name)
     settings_text = {
         "folders": [
@@ -40,6 +57,7 @@ def create_sublime_project(project_folder, project_name, interpreter, *args):
     }
     target_path = (os.path.join(project_folder, sublime_file_name))
 
+    # @todo: wrap it in a try-except block in case target_path doesn't exist
     with open(target_path, 'w') as f:
         f.write(json.dumps(settings_text, sort_keys=True, indent=4))
 
