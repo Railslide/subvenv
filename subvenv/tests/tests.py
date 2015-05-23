5597af4eed72084f5579ef0152eb6745b778cd89
Giulia Vergottini <hello@railslide.io>
2015-05-23 11:06:44 +0200

    testing for failures

--- subvenv/tests/tests.py
+++ subvenv/tests/tests.py
@@ -13,10 +13,31 @@ class SubvenvTests(unittest.TestCase):
     #     pass
 
     @patch.object(os, 'getenv', return_value='')
-    def test_postmkproject_without_env(self, os_mock):
+    def test_postmkproject_without_virtualenv(self, os_mock):
         """
-        Running post_mkproject without being in an env
+        Running post_mkproject without being in a virtualenv
         should exit the program.
         """
         with self.assertRaises(SystemExit):
             subvenv.post_mkproject()
+
+    def test_create_sublime_project_file_with_non_existing_target_folder(self):
+        """
+        Trying to create a sublime project file into a non
+        existing folder should exit the program.
+        """
+        with self.assertRaises(SystemExit):
+            subvenv.create_sublime_project_file(
+                'nonexistingfolder',
+                'test_name',
+                'test_interpreter'
+            )
+
+    @patch.object(os, 'getenv', return_value='')
+    def test_make_project_without_virtualenv(self, os_mock):
+        """
+        Running make_mkproject without being in a virtualenv
+        should exit the program.
+        """
+        with self.assertRaises(SystemExit):
+            subvenv.make_project()
